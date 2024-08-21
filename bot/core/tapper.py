61cdffb3bc1cacb1505958c17b4a0d476705f24e
Ihor Kohut <kohutiv@gmail.com>
2024-08-21 23:45:24 +0300

    test

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -542,7 +542,7 @@ class Tapper:
 
                                 await asyncio.sleep(delay=2)
 
-                                break
+                            break
 
                     await asyncio.sleep(delay=randint(6, 14))
 
