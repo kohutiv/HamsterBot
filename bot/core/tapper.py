4140555146bdbb471822a9308ffac3f5413c5b51
Ihor Kohut <kohutiv@gmail.com>
2024-08-21 18:59:08 +0300

    test

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -492,12 +492,6 @@ class Tapper:
 
                     all_keys = len(promo_states) * 4
 
-                    logger.info(
-                        f"{self.session_name} | <lg>{found_keys}</lg> keys out of <lg>{all_keys}</lg> are activated!")
-
-                    if found_keys >= (all_keys * settings.PER_ENTERED_KEYS) / 100:
-                        break
-
                     apps_info = [{"appToken": "74ee0b5b-775e-4bee-974f-63e7f4d5bacb",
                                   "promoId": "fe693b26-b342-4159-8808-15e3ff7f8767", "minWaitAfterLogin": 120},
                                  {"appToken": "d1690a07-3780-4068-810f-9b5bbf2931b2",
@@ -533,6 +527,12 @@ class Tapper:
 
                         if not app_token:
                             continue
+
+                        logger.info(
+                            f"{self.session_name} | <lg>{found_keys}</lg> keys out of <lg>{all_keys}</lg> are activated!")
+
+                        if found_keys >= (all_keys * settings.PER_ENTERED_KEYS) / 100:
+                            break
 
                         title = promo['title']['en']
                         keys_per_day = promo['keysPerDay']
