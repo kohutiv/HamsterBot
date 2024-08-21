ede5ddd88bbfa67de58baef816c709f1e916dc82
Ihor Kohut <kohutiv@gmail.com>
2024-08-21 19:06:43 +0300

    test

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -493,19 +493,26 @@ class Tapper:
                     all_keys = len(promo_states) * 4
 
                     apps_info = [{"appToken": "74ee0b5b-775e-4bee-974f-63e7f4d5bacb",
-                                  "promoId": "fe693b26-b342-4159-8808-15e3ff7f8767", "minWaitAfterLogin": 120},
+                                  "promoId": "fe693b26-b342-4159-8808-15e3ff7f8767",
+                                  "minWaitAfterLogin": 120},
                                  {"appToken": "d1690a07-3780-4068-810f-9b5bbf2931b2",
-                                  "promoId": "b4170868-cef0-424f-8eb9-be0622e8e8e3", "minWaitAfterLogin": 35},
+                                  "promoId": "b4170868-cef0-424f-8eb9-be0622e8e8e3",
+                                  "minWaitAfterLogin": 35},
                                  {"appToken": "82647f43-3f87-402d-88dd-09a90025313f",
-                                  "promoId": "c4480ac7-e178-4973-8061-9ed5b2e17954", "minWaitAfterLogin": 40},
+                                  "promoId": "c4480ac7-e178-4973-8061-9ed5b2e17954",
+                                  "minWaitAfterLogin": 40},
                                  {"appToken": "d28721be-fd2d-4b45-869e-9f253b554e50",
-                                  "promoId": "43e35910-c168-4634-ad4f-52fd764a843f", "minWaitAfterLogin": 45},
+                                  "promoId": "43e35910-c168-4634-ad4f-52fd764a843f",
+                                  "minWaitAfterLogin": 45},
                                  {"appToken": "8d1cc2ad-e097-4b86-90ef-7a27e19fb833",
-                                  "promoId": "dc128d28-c45b-411c-98ff-ac7726fbaea4", "minWaitAfterLogin": 50},
+                                  "promoId": "dc128d28-c45b-411c-98ff-ac7726fbaea4",
+                                  "minWaitAfterLogin": 50},
                                  {"appToken": "61308365-9d16-4040-8bb0-2f4a4c69074c",
-                                  "promoId": "61308365-9d16-4040-8bb0-2f4a4c69074c", "minWaitAfterLogin": 46},
+                                  "promoId": "61308365-9d16-4040-8bb0-2f4a4c69074c",
+                                  "minWaitAfterLogin": 46},
                                  {"appToken": "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71",
-                                  "promoId": "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71", "minWaitAfterLogin": 40}]
+                                  "promoId": "2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71",
+                                  "minWaitAfterLogin": 40}]
 
                     # apps_info = await get_apps_info(http_client=http_client)
                     apps = {
