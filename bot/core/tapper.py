41441b71adb36f370602cdcb6ce5488c18291554
Ihor Kohut <kohutiv@gmail.com>
2024-08-24 07:25:52 +0300

    🛠 Add wake up

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -585,7 +585,7 @@ class Tapper:
 
                                 await asyncio.sleep(delay=2)
 
-                                break
+                            break
 
                     await asyncio.sleep(delay=randint(6, 14))
 
