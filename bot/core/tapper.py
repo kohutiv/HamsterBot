293845cd0724e7713394ae55857ad32af903d4bf
Ihor Kohut <kohutiv@gmail.com>
2024-08-30 12:27:11 +0300

    🧱 Add mini game

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -625,7 +625,7 @@ class Tapper:
                                 "promoId": "c7821fa7-6632-482c-9635-2bd5798585f9",
                                 "appToken": "b6de60a0-e030-48bb-a551-548372493523",
                                 "minWaitAfterLogin": 31,
-                                "name": "Ganswar"
+                                "name": "Gangs Wars"
                             }
                         ]
 
