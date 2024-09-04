2a2e61a3b56e70e6d19633d94afc5bd2472b0b3d
Ihor Kohut <kohutiv@gmail.com>
2024-09-04 19:54:52 +0300

    🛠 add promoId

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -532,6 +532,7 @@ class Tapper:
                                                 f"Trying again...")
 
                                 await asyncio.sleep(delay=2)
+                            break
 
                     await asyncio.sleep(delay=randint(2, 4))
 
